d70274cb292645a95f0bcb6cab51b3b4603ae5ba
Cynthia Mugo <cynthiamugo25@gmail.com>
2025-07-10 23:56:49 +0300

    Add jsx to components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function NavBar() {
+  return (
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
+  <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>
+  );
+  //return <nav>NavBar</nav>;
 }
 
 export default NavBar;
